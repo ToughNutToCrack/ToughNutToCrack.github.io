def809c25fc8687aa355dd22982befb8d2ca49c0
Erik Minarini <minarini.work@gmail.com>
2019-01-24 23:19:28 +0100

    test for new site

---
+++ precache-manifest.b8d2bd68840573fa324690c24f87c056.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "b092c9c6ac47edda8a94e68e9d1f4a5a",
+    "url": "/static/media/Cloud1.b092c9c6.obj"
+  },
+  {
+    "revision": "e1f0fe5bc4af1b8b16b5",
+    "url": "/static/css/main.d83b9688.chunk.css"
+  },
+  {
+    "revision": "768533286215b8da9fe3",
+    "url": "/static/js/1.76853328.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bfc26faaf415d7ed94ef0a4b8daa7fcb",
+    "url": "/static/media/LogoSoloFacciaStudio.bfc26faa.png"
+  },
+  {
+    "revision": "7654cbdabd5077d7c019c220cabe28c6",
+    "url": "/static/media/ScrittaLogo.7654cbda.obj"
+  },
+  {
+    "revision": "cde5032ae75425c2ede84cd138e18386",
+    "url": "/static/media/ScrittaLogo.cde5032a.mtl"
+  },
+  {
+    "revision": "d62c9e3e2317c6e16f9a68133dc647e4",
+    "url": "/static/media/TorriMedie.d62c9e3e.obj"
+  },
+  {
+    "revision": "658fb562ce66b6465f4749b55e64ca2f",
+    "url": "/static/media/TorriMedie.658fb562.mtl"
+  },
+  {
+    "revision": "e1f0fe5bc4af1b8b16b5",
+    "url": "/static/js/main.e1f0fe5b.chunk.js"
+  },
+  {
+    "revision": "69781895633887837168f77a0396561b",
+    "url": "/static/media/Cloud1.69781895.mtl"
+  },
+  {
+    "revision": "8f5afe01c662d5142576625649c182c4",
+    "url": "/static/media/Cloud2.8f5afe01.obj"
+  },
+  {
+    "revision": "69781895633887837168f77a0396561b",
+    "url": "/static/media/Cloud2.69781895.mtl"
+  },
+  {
+    "revision": "ba89c60a45b873c73a1be399b00b7611",
+    "url": "/static/media/Cloud3.ba89c60a.obj"
+  },
+  {
+    "revision": "69781895633887837168f77a0396561b",
+    "url": "/static/media/Cloud3.69781895.mtl"
+  },
+  {
+    "revision": "49f574c69e46b4fe35cabb56fd21d931",
+    "url": "/static/media/LogoSoloFacciaBroadcast.49f574c6.png"
+  },
+  {
+    "revision": "4bc4f19cb0f5ef2df6ab03bf83fe1d23",
+    "url": "/static/media/Smoke-Element.4bc4f19c.png"
+  },
+  {
+    "revision": "5dbf74c2062394f8fdfc29597c534726",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bf3063e49c6aadee58521e9ebbc6c2c3.js"
+  "/precache-manifest.b8d2bd68840573fa324690c24f87c056.js"
 );
 
 workbox.clientsClaim();

7bc27eae3ba37147e8c469d9367b73ca7ef106b8
Erik Minarini <minarini.work@gmail.com>
2019-02-11 21:42:10 +0100

    test

---
+++ precache-manifest.7fedebaefb50d5da77c3c3da72b052ea.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "69781895633887837168f77a0396561b",
+    "url": "/static/media/Cloud2.69781895.mtl"
+  },
+  {
+    "revision": "c10cee0fbd98e3979d56",
+    "url": "/static/css/main.3bbb0183.chunk.css"
+  },
+  {
+    "revision": "011ae5690ae6f4f1dbb12bb01752c56a",
+    "url": "/static/media/YT.011ae569.jpg"
+  },
+  {
+    "revision": "d1f525fd691dd5ffa1d4",
+    "url": "/static/js/1.d1f525fd.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7654cbdabd5077d7c019c220cabe28c6",
+    "url": "/static/media/ScrittaLogo.7654cbda.obj"
+  },
+  {
+    "revision": "cde5032ae75425c2ede84cd138e18386",
+    "url": "/static/media/ScrittaLogo.cde5032a.mtl"
+  },
+  {
+    "revision": "6aef1e99587765f8358fa99c8b1f31be",
+    "url": "/static/media/TorriMedie.6aef1e99.obj"
+  },
+  {
+    "revision": "21f6d7a7ecd978896a3f137018ed8f8a",
+    "url": "/static/media/TorriMedie.21f6d7a7.mtl"
+  },
+  {
+    "revision": "b092c9c6ac47edda8a94e68e9d1f4a5a",
+    "url": "/static/media/Cloud1.b092c9c6.obj"
+  },
+  {
+    "revision": "69781895633887837168f77a0396561b",
+    "url": "/static/media/Cloud1.69781895.mtl"
+  },
+  {
+    "revision": "8f5afe01c662d5142576625649c182c4",
+    "url": "/static/media/Cloud2.8f5afe01.obj"
+  },
+  {
+    "revision": "c10cee0fbd98e3979d56",
+    "url": "/static/js/main.c10cee0f.chunk.js"
+  },
+  {
+    "revision": "ba89c60a45b873c73a1be399b00b7611",
+    "url": "/static/media/Cloud3.ba89c60a.obj"
+  },
+  {
+    "revision": "69781895633887837168f77a0396561b",
+    "url": "/static/media/Cloud3.69781895.mtl"
+  },
+  {
+    "revision": "49f574c69e46b4fe35cabb56fd21d931",
+    "url": "/static/media/LogoSoloFacciaBroadcast.49f574c6.png"
+  },
+  {
+    "revision": "bfc26faaf415d7ed94ef0a4b8daa7fcb",
+    "url": "/static/media/LogoSoloFacciaStudio.bfc26faa.png"
+  },
+  {
+    "revision": "a85bc3a507bf5a4327af8e9158d2652b",
+    "url": "/static/media/Greetings.a85bc3a5.png"
+  },
+  {
+    "revision": "4bc4f19cb0f5ef2df6ab03bf83fe1d23",
+    "url": "/static/media/Smoke-Element.4bc4f19c.png"
+  },
+  {
+    "revision": "fdcb16ae6d02c6cb706c4925c2c87b98",
+    "url": "/static/media/Bologna.fdcb16ae.jpg"
+  },
+  {
+    "revision": "d702d0f1e91a14bbc2b8391597e3c29b",
+    "url": "/static/media/GameJam.d702d0f1.png"
+  },
+  {
+    "revision": "b1c4d91934eac70f5e4018dedcf73720",
+    "url": "/static/media/Course.b1c4d919.jpg"
+  },
+  {
+    "revision": "d1f525fd691dd5ffa1d4",
+    "url": "/static/css/1.c7dd7257.chunk.css"
+  },
+  {
+    "revision": "174301a8b58ff11ab565ef5af786bd61",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3dc44fa9b644a171cdd37cf422d94d32.js"
+  "/precache-manifest.7fedebaefb50d5da77c3c3da72b052ea.js"
 );
 
 workbox.clientsClaim();

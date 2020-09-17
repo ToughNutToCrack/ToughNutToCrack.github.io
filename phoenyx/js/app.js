86afb75071fcf29179e371f98a52659694fc4ab0
Matteo <matteo@tntc.it>
2020-09-17 11:45:26 +0200

    phoenyx with video

--- phoenyx/js/app.js
+++ phoenyx/js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/WEBGLBuild.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/BuildWEBGL.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;

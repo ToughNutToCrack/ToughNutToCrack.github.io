1c81938af98df309c985b11ce9deb1c65cc306a0
Matteo Baraldi <matteo@tntc.it>
2024-06-12 16:23:33 +0200

    folder structure + base 64 image

--- businesscard/erik/script.js
+++ businesscard/erik/script.js
@@ -2,12 +2,12 @@ function downloadVCard() {
     const vCardData = `
 BEGIN:VCARD
 VERSION:3.0
-FN:Matteo Baraldi
-N:Baraldi;Matteo;;;
+FN:Erik Minarini
+N:Minarini;Erik;;;
 ORG:TNTC
 TITLE:Co-founder, Senior Software Developer
 EMAIL;TYPE=work:info@tntc.it
-TEL;TYPE=work:+393496991261
+TEL;TYPE=work:+393427027834
 URL:http://www.tntc.it
 NOTE:TNTC STUDIO is composed of a team of highly specialized experts capable of crafting custom apps that bring your ideas to life. Our expertise spans various domains, including Virtual & Augmented Reality (XR), Videogames, 3D Visualization, 3D Websites, and beyond. In four words: We love real-time 3D! Feel free to contact us to learn more about our services or request a complimentary quotation.
 END:VCARD`;
@@ -15,7 +15,7 @@ END:VCARD`;
     const blob = new Blob([vCardData], { type: 'text/vcard;charset=utf-8' });
     const link = document.createElement('a');
     link.href = URL.createObjectURL(blob);
-    link.download = 'matteo_baraldi.vcf';
+    link.download = 'erik_minarini.vcf';
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
